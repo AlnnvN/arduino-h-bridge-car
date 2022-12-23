b34a4b29ef8a7d90a0b322478ec7e3a9a79413fa
Alan Nascimento <alanvan3791@gmail.com>
2022-12-22 21:15:13 -0300

    pin schema documented on main file

--- arduino-h-bridge-car.ino
+++ arduino-h-bridge-car.ino
@@ -11,19 +11,19 @@ void setup()
   
   //PINS SETUP
   //H-bridge
-  car.getMotor1().setEnablePin(5);
-  car.getMotor1().setThrottlePin(6);
-  car.getMotor1().setReversePin(7);
+  car.getMotor1().setEnablePin(5); //PWM, Analog Output - H-brige, Activate 1 & 2 (1st motor, both ways)
+  car.getMotor1().setThrottlePin(6); //Digital Output - H-bridge, Input 1 
+  car.getMotor1().setReversePin(7); //Digital Output - H-bridge, Input 2 
   
-  car.getMotor2().setEnablePin(11);
-  car.getMotor2().setThrottlePin(12);
-  car.getMotor2().setReversePin(13);
+  car.getMotor2().setEnablePin(11); //PWM, Analog Output - H-brige, Activate 3 & 4 (2nd motor, both ways)
+  car.getMotor2().setThrottlePin(12); //Digital Output - H-bridge, Input 3
+  car.getMotor2().setReversePin(13); //Digital Output - H-bridge, Input 4
   
-  //Controller
-  controller.setThrottleBtnPin(A0);
-  controller.setReverseBtnPin(A1);
-  controller.setLeftBtnPin(A2);
-  controller.setRightBtnPin(A3);
+  //Controller - Analog Input
+  controller.setThrottleBtnPin(A0); //INPUT_PULLUP
+  controller.setReverseBtnPin(A1); //INPUT_PULLUP
+  controller.setLeftBtnPin(A2); //INPUT_PULLUP
+  controller.setRightBtnPin(A3); //INPUT_PULLUP
   controller.setPotentiometerPin(A4);
 
 }
@@ -36,16 +36,16 @@ void loop()
   
   //CHECKS FOR BUTTON INPUT FOR ANALOG UPDATE
   //pins set for INPUT_PULLUP - if it reads 0, it's pressed.
-  if(analogRead(controller.getThrottleBtnPin())<200){ //throttle input
+  if(analogRead(controller.getThrottleBtnPin())<200){ //Checks for throttle input
     car.throttle(speed);
   }
-  else if(analogRead(controller.getReverseBtnPin())<200){ //reverse input
+  else if(analogRead(controller.getReverseBtnPin())<200){ //Checks for reverse input
   	car.reverse(speed);
   }
-  else if(analogRead(controller.getLeftBtnPin())<200){ //left input
+  else if(analogRead(controller.getLeftBtnPin())<200){ //Checks for left input
   	car.turnLeft(speed);
   }
-  else if(analogRead(controller.getRightBtnPin())<200){ //right input
+  else if(analogRead(controller.getRightBtnPin())<200){ //Checks for right input
   	car.turnRight(speed);
   }
   else{
